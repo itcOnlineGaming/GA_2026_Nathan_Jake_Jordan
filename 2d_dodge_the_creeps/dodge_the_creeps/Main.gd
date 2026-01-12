9fbf5796ef73cbad9bc040c8a4f946314511e3c2
Jake Fitzgerald <115044918+Jake-Fitzgerald@users.noreply.github.com>
2026-01-12 15:01:07 +0000

    Added CSV and integrated powerup timer based on 50/50 choice in the beginning of the game when Start button is pressed

--- 2d_dodge_the_creeps/dodge_the_creeps/Main.gd
+++ 2d_dodge_the_creeps/dodge_the_creeps/Main.gd
@@ -4,13 +4,56 @@ extends Node
 @export var powerup: RigidBody2D
 var score
 
+#Var to print
+var player_speed = 0;
+var enemy_speed = 100.0;
+var time_count = 0.0;
+var powerup_spawn_timer = 30.0;
+
+var powerup_timer_hard = 15;
+var powerup_timer_medium = 10;
+var timer_picked = 0;
+
+func get_score_timer():
+	var score_timer: Timer = $ScoreTimer
+	time_count = score
+	print(time_count);
+	
+func change_var_on_conditions():
+	powerup_spawn_timer = timer_picked;
+
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
 	$Music.stop()
 	$DeathSound.play()
+	get_score_timer();
+	write_statistics();
 
+func write_statistics():
+	var path = "user://statistics.csv"
+	print(OS.get_user_data_dir())
+	#var file = FileAccess.open("res://statistics.csv", FileAccess.WRITE)
+	var file = FileAccess;
+	# Check if file path is valid
+	if (FileAccess.file_exists(path)):
+		file = FileAccess.open(path, FileAccess.READ_WRITE);
+		file.seek_end(); # Go to end of file
+	else:
+		file = FileAccess.open(path, FileAccess.WRITE)
+		file.store_csv_line(["Timestamp", "Condition", "Survival", "ValueTime", "Powerup", "ValuePowerup"]) # Heading
+	
+	var run_id : int = Time.get_unix_time_from_system();
+
+	
+	# Timers
+	if (timer_picked == 15.0):
+		file.store_csv_line([run_id, "A", "Survival Time", time_count, "Powerup Timer rate", powerup_spawn_timer])
+	else:
+		file.store_csv_line([run_id, "B", "Survival Time", time_count, "Powerup Timer rate", powerup_spawn_timer])
+	
+	file.close()
 
 func new_game():
 	get_tree().call_group(&"mobs", &"queue_free")
@@ -21,6 +64,11 @@ func new_game():
 	$HUD.show_message("Get Ready")
 	$Music.play()
 	powerup.remove_powerup();
+	timer_picked = choose_a_or_b(powerup_timer_hard, powerup_timer_medium);
+	change_var_on_conditions();
+
+func choose_a_or_b(a, b):
+	return a if randi() % 2 == 0 else b
 
 
 func _on_MobTimer_timeout():
@@ -52,7 +100,7 @@ func _on_MobTimer_timeout():
 func _on_ScoreTimer_timeout():
 	score += 1
 	$HUD.update_score(score)
-	if (score % 5 == 0 && score != 0 && !powerup.powerupSpawned):
+	if (score % timer_picked == 0 && score != 0 && !powerup.powerupSpawned):
 		var powerupPos = Vector2(100,100); # Needs to be random
 		powerup.add_powerup(powerupPos);
 
